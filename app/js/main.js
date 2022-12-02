a93b40cd54691f28d7266ed20cf9179501d4158d
Weadmead <wead758@gmail.com>
2022-12-02 06:16:41 +0300

    start adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,18 @@
 $(function () {
 
 
+  const burger = document.querySelector('.header__burger');
+  const navMenu = document.querySelector('.header__menu');
 
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+  })
+
+  document.querySelectorAll('.header__menu-link').forEach(n => n.addEventListener('click', () => {
+    burger.classList.remove('active');
+    navMenu.classList.remove('active');
+  }))
 
       
 
