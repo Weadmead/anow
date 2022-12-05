a2678363ddf8c11e491a7cee21c56454bba772e9
Weadmead <wead758@gmail.com>
2022-12-05 03:22:47 +0300

    added animation

--- app/js/main.js
+++ app/js/main.js
@@ -7,13 +7,24 @@ $(function () {
   burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     navMenu.classList.toggle('active');
-  })
+  });
 
   document.querySelectorAll('.header__menu-link').forEach(n => n.addEventListener('click', () => {
     burger.classList.remove('active');
     navMenu.classList.remove('active');
-  }))
+  }));
 
+
+  const wow = new WOW(
+    {
+    boxClass:     'wow',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  );
+  wow.init();
       
 
  
--- gulpfile.js
+++ gulpfile.js
@@ -41,6 +41,7 @@ function images() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/wowjs/dist/wow.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
